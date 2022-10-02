37ab794001318bff119e7c5151956c7f81abd0fd
Gustavo <guto.h.d@hotmail.com>
2022-10-01 23:49:42 -0300

    Index e service de codigoParada criado

---
+++ ProjetoGestaoProducao/GestaoProducao_MVC/Controllers/CodigoParadasController.cs
@@ -0,0 +1,27 @@
+﻿using GestaoProducao_MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoProducao_MVC.Controllers
+{
+    public class CodigoParadasController : Controller
+
+    {
+
+        private readonly CodigoParadaService _codigoParadaService;
+
+        public CodigoParadasController(CodigoParadaService codigoParadaService)
+        {
+            _codigoParadaService = codigoParadaService;
+        }
+
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var list = await _codigoParadaService.FindByNameCodeAsync(searchString);
+
+            return View(list);
+        }
+
+
+    }
+}
---
+++ ProjetoGestaoProducao/GestaoProducao_MVC/Services/CodigoParadaService.cs
@@ -0,0 +1,99 @@
+﻿using GestaoProducao_MVC.Data;
+using GestaoProducao_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoProducao_MVC.Services
+{
+    public class CodigoParadaService
+    {
+        private readonly GestaoProducao_MVCContext _context;
+
+        public CodigoParadaService(GestaoProducao_MVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CodigoParada>> FindAllAsync()
+        {
+            return await _context.CodigoParada.ToListAsync();
+        } 
+
+
+        public async Task<CodigoParada> FindByIdAsync(int id)
+        {
+            return await _context.CodigoParada.FirstOrDefaultAsync(x => x.Id == id);
+
+        }
+
+        public async Task<List<CodigoParada>> FindByNameCodeAsync(string searchString)
+        {
+            var result = from obj in _context.CodigoParada select obj;
+
+            
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                result = result.Where(x => x.Descricao.Contains(searchString) || x.Id.ToString() == searchString);
+
+            }
+
+
+            return await result.ToListAsync();
+        }
+
+
+        public async Task InserAsync(CodigoParada obj)
+        {
+            _context.CodigoParada.Add(obj);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            try
+            {
+                var obj = await _context.CodigoParada.FindAsync(id);
+
+                _context.CodigoParada.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+               //Tratar esse erro
+            }
+        }
+
+
+        public async Task UpdateAsync(CodigoParada obj)
+        {
+            bool seExiste = await _context.CodigoParada.AnyAsync(x => x.Id == obj.Id);
+            if (!seExiste)
+            {
+                //se não existe retorna nao encontrado
+                throw new Exception("Elemento nao encontrado!!!");
+
+            }
+            try
+            {
+                _context.CodigoParada.Update(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbUpdateConcurrencyException(e.Message);
+
+            }
+        }
+
+        public async Task<bool> isExist(int? id)
+        {
+            if (id == null)
+            {
+                return false;
+            }
+
+            return await _context.CodigoParada.AnyAsync(x => x.Id == id.Value);
+        }
+    }
+
+
+}
+
