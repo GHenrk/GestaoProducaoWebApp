289560e699de3b880e64206fab0908670d1b733d
Gustavo <guto.h.d@hotmail.com>
2022-10-12 18:46:33 -0300

    pesquisa e filtro na dash

--- ProjetoGestaoProducao/GestaoProducao_MVC/wwwroot/js/dashboard.js
+++ ProjetoGestaoProducao/GestaoProducao_MVC/wwwroot/js/dashboard.js
@@ -6,8 +6,7 @@
 //});
 
 
-
-
+let listaMaquinas;
 
 async function requisitaDados() {
     //mostraLoading(true);
@@ -16,8 +15,9 @@ async function requisitaDados() {
         .then((response) => response.json())
         .then((data) => {
             console.log("Informações recebidas");
-            mostraLista(data);
-            console.log(data);
+            listaMaquinas = data;
+            mostraLista(listaMaquinas);
+
         })
         .catch((error) => {
             console.log("Deu erro" + error);
@@ -33,19 +33,10 @@ requisitaDados();
 
 function mostraLista(data) {
     var divLista = document.getElementById('divMaquinas');
-
-
-    //    for (let i = 0; i < data.length; i++) {
-    //        divLista.innerHTML += `<div class="card card-escuro mb-3" style="max-width: 20rem;">
-    //  <div class="card-header">${data[i].nome}</div>
-    //  <div class="card-body">
-    //    <h4 class="card-title"></h4>
-    //    <p class="card-text">teste.</p>
-    //<a href="/Maquinas/Details/${data[i].id}" class="btn btn-sm btn-outline-dark">Máquina Detalhes</a>
-    //  </div>`
-    //    }
+    divLista.innerHTML = " ";
 
     for (let i = 0; i < data.length; i++) {
+
         var card = document.createElement('div');
         card.classList.add('cardMaquina');
 
@@ -60,7 +51,7 @@ function mostraLista(data) {
         if (data[i].maquinaAtiva == true) {
             const tempoTotal = [data[i].totalHoras, data[i].totalMinutos, data[i].totalSegundos]
             console.log(tempoTotal)
-            
+
             var codigoProcesso = document.createElement('p');
             codigoProcesso.classList.add('codigoProcesso--card');
             codigoProcesso.append(`Apontamento: ${data[i].aptMaquina}`)
@@ -71,7 +62,7 @@ function mostraLista(data) {
 
             var tempoDecorrido = document.createElement('p');
             tempoDecorrido.classList.add('tempoTotal--card');
-            tempoDecorrido.id = i;
+            tempoDecorrido.id = data[i].id;
             tempoDecorrido.append(`${tempoTotal[0]}:${tempoTotal[1]}:${tempoTotal[2]}`);
 
             var quantidadeParadas = document.createElement('p');
@@ -84,7 +75,7 @@ function mostraLista(data) {
             detalhesCard.appendChild(quantidadeParadas);
             detalhesCard.appendChild(tempoDecorrido);
 
-            mostraTempo(tempoTotal, i);
+            mostraTempo(tempoTotal, data[i].id);
         }
 
 
@@ -110,13 +101,12 @@ function mostraLista(data) {
 
     }
 
-
+    criaCarrosel();
 };
 
 
 
 function mostraTempo(tempoTotal, idMaquina) {
-
     var horas = tempoTotal[0];
     var minutos = tempoTotal[1]
     var segundos = tempoTotal[2]
@@ -134,16 +124,100 @@ function mostraTempo(tempoTotal, idMaquina) {
             horas += 1;
             minutos = 0;
         }
-        
+
         horasFormat = horas < 10 ? "0" + horas : horas;
         minutosFormat = minutos < 10 ? "0" + minutos : minutos;
         segundosFormat = segundos < 10 ? "0" + segundos : segundos;
         var tempoFormatado = `${horasFormat}:${minutosFormat}:${segundosFormat}`;
         tempoTotalDisplay.append(tempoFormatado);
     }, 1000);
-    
 
-    
+
+
 
 }
 
+
+function resetaIntervals() {
+    // Get a reference to the last interval + 1
+    const interval_id = window.setInterval(function () { }, Number.MAX_SAFE_INTEGER);
+
+    // Clear any timeout/interval up to that id
+    for (let i = 1; i < interval_id; i++) {
+        window.clearInterval(i);
+    }
+}
+
+
+
+function criaCarrosel() {
+
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        rows: 2,
+        slidesPerRow: 3,
+        variableWidth: false,
+        adaptiveHeight: false
+
+    });
+}
+
+function destroiCarrosel() {
+    $('.carousel').slick('unslick');
+}
+
+
+const filtroOcioso = (elemento) => {
+    return elemento.status.toLowerCase() == "ocioso";
+}
+
+
+const filtroParado = (elemento) => {
+    return elemento.status.toLowerCase() == "parado";
+}
+
+const filtroAtivo = (elemento) => {
+    return elemento.status.toLowerCase() == "ativo";
+}
+
+const mostrarTodos = (elemento) => {
+    return elemento;
+}
+
+const filtros = {
+    "mostrarTodos": mostrarTodos,
+    "filtroAtivo": filtroAtivo,
+    "filtroParado": filtroParado,
+    "filtroOcioso": filtroOcioso
+}
+
+
+let searchName = document.getElementById('searchString');
+
+searchName.onkeyup = () => {
+    destroiCarrosel();
+
+    var listaMaquinasFiltrada = listaMaquinas.filter(filtroNome);
+
+    console.log(listaMaquinasFiltrada)
+    resetaIntervals();
+    mostraLista(listaMaquinasFiltrada);
+}
+
+const filtroNome = (elemento) => {
+    return elemento.nome.toLowerCase().includes(searchName.value);
+}
+
+
+const listaOpcoes = document.getElementById('opcoes');
+
+listaOpcoes.onchange = () => {
+    destroiCarrosel()
+    let listaMaquinasFiltrada = listaMaquinas.filter(filtros[listaOpcoes.value]);
+    resetaIntervals();
+    mostraLista(listaMaquinasFiltrada);
+}
